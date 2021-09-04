ea88520022dde367bda4e87281733e72bb6024a0
jeffnm <jeffmudge@gmail.com>
2021-09-04 10:35:41 -0500

    Initial stab at card functions

--- src/Cards.elm
+++ src/Cards.elm
@@ -1,23 +1,40 @@
 module Cards exposing (..)
 
+import List.Extra exposing (unique)
 import Types exposing (..)
+import Words exposing (words)
 
 
-words : List String
-words =
-    Debug.todo "Get a list of words"
+generateWords : Int -> List String -> List String
+generateWords gridSize wordList =
+    case gridSize of
+        5 ->
+            shuffleList wordList
+                |> List.Extra.unique
+                |> List.take 25
+
+        _ ->
+            Debug.todo "Get a list of words"
 
 
 pickTeams : Int -> List Team
 pickTeams gridSize =
     case gridSize of
+        5 ->
+            -- 25 cards
+            List.repeat 6 Red
+                |> List.append (List.repeat 6 Blue)
+                |> List.append (List.repeat 12 Gray)
+                |> List.append (List.singleton Assassin)
+                |> shuffleList
+
         _ ->
             Debug.todo "List of Teams"
 
 
 generateCards : List String -> List Team -> List Card
 generateCards wordList teamsList =
-    Debug.todo "Generate Cards"
+    List.map2 (\w t -> Card w t False) wordList teamsList
 
 
 shuffleList : List a -> List a
---
+++ src/Words.elm
@@ -0,0 +1,11 @@
+module Words exposing (..)
+
+
+words =
+    [ "Words"
+    , "Egypt"
+    , "Mars"
+    , "Spades"
+    , "Mathematics"
+    , "Unique"
+    ]
